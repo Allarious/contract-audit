87a91975d18ea0227ed21702cbdc28f92d575f8d
allarious <arjmands@ualberta.ca>
2022-07-20 16:10:42 -0600

    added new compiler version for openzeppelin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { nodeAPIKey } from "./config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9"
+      },
+      {
+        version: "0.6.0"
+      }
+    ]
+  },
+  networks:{
+    hardhat:{
+      forking:{
+        url: nodeAPIKey
+      }
+    }
+  }
 };
 
 export default config;
