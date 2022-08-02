4d7d044a3a582cba3defa85b83efc4a4d5eeeaf6
allarious <arjmands@ualberta.ca>
2022-08-02 17:09:01 -0600

    added the test to run gatekeeper test

--- test/GatekeeperOne.test.ts
+++ test/GatekeeperOne.test.ts
@@ -12,7 +12,10 @@ describe("Exploit GateKeeperOne", function() {
         const gatekeeperAddress = await gateKeeper.address
         const gatekeeperExploiter = await GatekeeperExploiter.deploy(gatekeeperAddress);
 
-        return {gateKeeper, gatekeeperExploiter}
+        const instance1 = await gateKeeper.deployed();
+        const instance2 = await gatekeeperExploiter.deployed();
+
+        return {gateKeeper: instance1, gatekeeperExploiter: instance2}
     }
 
     it("Should have the address of the owner set to the creator", async () => {
@@ -20,5 +23,11 @@ describe("Exploit GateKeeperOne", function() {
         const [owner] = await hre.ethers.getSigners();
         expect(await gatekeeperExploiter.ownerAddress()).to.equal(owner.address)
     })
+    
+    xit("Should be able to exploit the gateKeeperOne contract", async () => {
+        const {gateKeeper, gatekeeperExploiter} = await loadFixture(deployGateKeeper);
 
+        const isEntered = await gatekeeperExploiter.functions.exploit("0x0000000000000000", {gasLimit: 3000000});
+        // expect(isEntered).to.equal(true);
+    })
 })
