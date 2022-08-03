64fc4157eca964d9735d86f52433fb7798ec2df3
allarious <arjmands@ualberta.ca>
2022-08-03 13:25:19 -0600

    added console.logs

--- contracts/GateKeeperTwo.sol
+++ contracts/GateKeeperTwo.sol
@@ -9,19 +9,26 @@ contract GatekeeperTwo {
   address public entrant;
 
   modifier gateOne() {
+    console.log("Start of Gate 1");
     require(msg.sender != tx.origin);
+    console.log("End of Gate 1");
     _;
   }
 
   modifier gateTwo() {
+    console.log("Start of Gate 2");
     uint x;
     assembly { x := extcodesize(caller()) }
+    console.log("The size of the contract is: %s", x);
     require(x == 0);
+    console.log("End of Gate 2");
     _;
   }
 
   modifier gateThree(bytes8 _gateKey) {
+    console.log("Start of Gate 3");
     require(uint64(bytes8(keccak256(abi.encodePacked(msg.sender)))) ^ uint64(_gateKey) == uint64(0) - 1);
+    console.log("End of Gate 3");
     _;
   }
 
