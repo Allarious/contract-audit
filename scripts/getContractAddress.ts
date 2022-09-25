5ac17759383cb4cecbf61bd40a7c9cd0b96e9049
allarious <arjmands@ualberta.ca>
2022-09-24 19:12:04 -0600

    added base for alilen codex

--- scripts/getContractAddress.ts
+++ scripts/getContractAddress.ts
@@ -1,4 +1,4 @@
 
 import { ethers } from "hardhat";
 
-console.log(ethers.utils.getContractAddress({from: "0xd81d19ce53621c5da0f997830c1b97fb872c2daa", nonce: 144}))
+console.log(ethers.utils.getContractAddress({from: "0xFe9F7511D2f52709C473632b50fa5B3797a7C777", nonce: 1}))
